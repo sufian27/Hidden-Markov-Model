67193daeaab4d6b15bf6503de2d4bb09071e3f38
Tianyi <inipop.tianyi@gmail.com>
2021-04-24 12:25:07 -0400

    added forward-backward algorithm implementation

--- starterCode/hmm.py
+++ starterCode/hmm.py
@@ -1,6 +1,7 @@
 # File: hmm.py
 # Purpose:  Starter code for building and training an HMM in CSC 246.
 
+
 import os
 import argparse
 import numpy as np
@@ -37,15 +38,43 @@ class HMM:
         self.pi=np.random.rand(self.num_states)
         #TEMPORARY: Intializing emissions to random numbers between 0 and 1
         self.emissions=np.random.rand(self.num_states, self.vocab_size)
-
+    
     # return the loglikelihood for a complete dataset (train OR test) (list of matrices)
     def loglikelihood(self, dataset):
         pass
-
+    
     # return the loglikelihood for a single sequence (numpy matrix)
     def loglikelihood_helper(self, sample):
         pass
 
+    # given the integer representation of a single sequence
+    # return a T x num_states matrix of alpha where T is the total number of tokens in a single sequence
+    def forward(self, sample):
+        alpha = np.zeros((len(sample), self.num_states))
+        # initialization
+        for j in range(0, self.num_states):
+            alpha[0][j] = self.pi[j] * self.emissions[j][sample[0]]
+        # recursion
+        for t in range(1, len(sample)):
+            for j in range(0, self.num_states):
+                for i in range(0, self.num_states):
+                    alpha[t][j] += alpha[t-1][i] * self.transitions[i][j] * self.emissions[j][sample[t]]
+        return alpha
+
+    # given the integer representation of a single sequence
+    # return a T x num_state matrix of beta where T is the total number of tokens in a single sequence
+    def backward(self, sample):
+        beta = np.zeros((len(sample), self.num_states))
+        # initialization
+        for i in range(0, self.num_states):
+            beta[len(sample)-1][j] = 1
+        # recursion
+        for t in range(1, len(sample)):
+            for i in range(0, self.num_states):
+                for j in range(0, self.num_states):
+                    beta[len(sample)-1-t][i] += self.transitions[i][j] * self.emissions[j][sample[len(sample)-t]] * beta[len(sample)-t][j]
+        return beta
+    
     # apply a single step of the em algorithm to the model on all the training data,
     # which is most likely a python list of numpy matrices (one per sample).
     # Note: you may find it helpful to write helper methods for the e-step and m-step,
@@ -58,17 +87,13 @@ class HMM:
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Program to build and train a neural network.')
-    parser.add_argument('--train_path', default=None,
-                        help='Path to the training data directory.')
-    parser.add_argument('--dev_path', default=None,
-                        help='Path to the development data directory.')
-    parser.add_argument('--max_iters', type=int, default=30,
-                        help='The maximum number of EM iterations (default 30)')
-    parser.add_argument('--hidden_states', type=int, default=10,
-                        help='The number of hidden states to use. (default 10)')
+    parser = argparse.ArgumentParser(description='Program to build and train a neural network.')
+    parser.add_argument('--train_path', default=None, help='Path to the training data directory.')
+    parser.add_argument('--dev_path', default=None, help='Path to the development data directory.')
+    parser.add_argument('--max_iters', type=int, default=30, help='The maximum number of EM iterations (default 30)')
+    parser.add_argument('--hidden_states', type=int, default=10, help='The number of hidden states to use. (default 10)')
     args = parser.parse_args()
+
 
     # OVERALL PROJECT ALGORITHM:
     # 1. load training and testing data into memory
@@ -86,20 +111,6 @@ def main():
     # 5+. use EM to train the HMM on the training data,
     #     output loglikelihood on train and test after each iteration
     #     if it converges early, stop the loop and print a message
-    
-    #Paths for positive and negative training data
-    postrain= os.path.join(args.train_path, 'pos')
-    negtrain= os.path.join(args.train_path, 'neg')
-    #Combine into list
-    train_paths= [postrain, negtrain]
-    
-    #Create vocab and get its size. word_vocab is a dictionary from words to integers. Ex: 'painful':2070
-    word_vocab = build_vocab_words(train_paths)
-    vocab_size = len(word_vocab)
-    
-    #Create model
-    model = HMM(args.hidden_states, vocab_size)
-
 
 if __name__ == '__main__':
     main()
