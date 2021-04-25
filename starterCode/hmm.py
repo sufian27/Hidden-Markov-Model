f524ebe4dac6ae4752d6e90aba1cfa43ab6e7efc
sufian27 <sufianmushtaq@gmail.com>
2021-04-25 14:47:42 -0400

    e_step complete

--- starterCode/hmm.py
+++ starterCode/hmm.py
@@ -7,7 +7,6 @@ import argparse
 import numpy as np
 from nlputil import *   # utility methods for working with text
 
-
 # A utility class for bundling together relevant parameters - you may modify if you like.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # num_states -- this should be an integer recording the number of hidden states
@@ -22,27 +21,30 @@ from nlputil import *   # utility methods for working with text
 # vocab_size -- this should be an integer recording the vocabulary size
 #
 # Note: You may want to add fields for expectations.
+
+
 class HMM:
     __slots__ = ('pi', 'transitions', 'emissions', 'num_states', 'vocab_size')
 
     # The constructor should initalize all the model parameters.
     # you may want to write a helper method to initialize the emission probabilities.
     def __init__(self, num_states, vocab_size):
-        #Num_states is number of hidden states
-        self.num_states=num_states
-        #Vocab size is the number of unique words
-        self.vocab_size=vocab_size
-        #Transitions are a KxK matrix where K is the number of hidden states. Initialized randomly between 0 and 1
-        self.transitions=np.random.rand(self.num_states,self.num_states)
-        #pi is vector of size K, also initialized between 0 and 1
-        self.pi=np.random.rand(self.num_states)
-        #TEMPORARY: Intializing emissions to uniform distribution
-        self.emissions=np.random.uniform(size=(self.num_states, self.vocab_size))
-    
+        # Num_states is number of hidden states
+        self.num_states = num_states
+        # Vocab size is the number of unique words
+        self.vocab_size = vocab_size
+        # Transitions are a KxK matrix where K is the number of hidden states. Initialized randomly between 0 and 1
+        self.transitions = np.random.rand(self.num_states, self.num_states)
+        # pi is vector of size K, also initialized between 0 and 1
+        self.pi = np.random.rand(self.num_states)
+        # TEMPORARY: Intializing emissions to uniform distribution
+        self.emissions = np.random.uniform(
+            size=(self.num_states, self.vocab_size))
+
     # return the loglikelihood for a complete dataset (train OR test) (list of matrices)
     def loglikelihood(self, dataset):
         pass
-    
+
     # return the loglikelihood for a single sequence (numpy matrix)
     def loglikelihood_helper(self, sample):
         pass
@@ -58,7 +60,8 @@ class HMM:
         for t in range(1, len(sample)):
             for j in range(0, self.num_states):
                 for i in range(0, self.num_states):
-                    alpha[t][j] += alpha[t-1][i] * self.transitions[i][j] * self.emissions[j][sample[t]]
+                    alpha[t][j] += alpha[t-1][i] * \
+                        self.transitions[i][j] * self.emissions[j][sample[t]]
         return alpha
 
     # given the integer representation of a single sequence
@@ -72,14 +75,44 @@ class HMM:
         for t in range(1, len(sample)):
             for i in range(0, self.num_states):
                 for j in range(0, self.num_states):
-                    beta[len(sample)-1-t][i] += self.transitions[i][j] * self.emissions[j][sample[len(sample)-t]] * beta[len(sample)-t][j]
+                    beta[len(sample)-1-t][i] += self.transitions[i][j] * \
+                        self.emissions[j][sample[len(
+                            sample)-t]] * beta[len(sample)-t][j]
         return beta
-    
+
+    # Uses alpha and beta values to calculate
+    # e[t][i][j] = Probability of being in state i at time t and state j at time t+1
+    # y[t][j] = Probability of being in state j at time t
+    def e_step(self, sample):
+        alpha = self.forward(sample)
+        beta = self.backward(sample)
+        y = np.zeros((len(sample), self.num_states))
+        e = np.zeros((len(sample), self.num_states, self.num_states))
+        for t in range(1, len(sample)):
+            den = 0
+            for j in range(0, self.num_states):
+                den += alpha[t][j] * beta[t][j]
+            for j in range(0, self.num_states):
+                y[t][j] = (alpha[t][j] * beta[t][j])/den
+                for i in range(0, self.num_states):
+                    if t != len(sample) - 1:
+                        e[t][j][i] = (alpha[t][i] * self.transitions[i]
+                                      [j] * self.emissions[j][t+1] * beta[t+1][j])/den
+        return y, e
+
+    # Uses the e and y matrices from the e_step to tune transition and emission probabilities
+    def m_step(self, y, e):
+        pass
+
     # apply a single step of the em algorithm to the model on all the training data,
     # which is most likely a python list of numpy matrices (one per sample).
     # Note: you may find it helpful to write helper methods for the e-step and m-step,
     def em_step(self, dataset):
-        pass
+        # Takes out a sample from the dataset and does e_step and m_step
+        for sample in dataset:
+            y, e = e_step(sample)
+            m_step(y, e)
+            break
 
     # Return a "completed" sample by additing additional steps based on model probability.
     def complete_sequence(self, sample, steps):
@@ -87,13 +120,17 @@ class HMM:
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Program to build and train a neural network.')
-    parser.add_argument('--train_path', default=None, help='Path to the training data directory.')
-    parser.add_argument('--dev_path', default=None, help='Path to the development data directory.')
-    parser.add_argument('--max_iters', type=int, default=30, help='The maximum number of EM iterations (default 30)')
-    parser.add_argument('--hidden_states', type=int, default=10, help='The number of hidden states to use. (default 10)')
+    parser = argparse.ArgumentParser(
+        description='Program to build and train a neural network.')
+    parser.add_argument('--train_path', default=None,
+                        help='Path to the training data directory.')
+    parser.add_argument('--dev_path', default=None,
+                        help='Path to the development data directory.')
+    parser.add_argument('--max_iters', type=int, default=30,
+                        help='The maximum number of EM iterations (default 30)')
+    parser.add_argument('--hidden_states', type=int, default=10,
+                        help='The number of hidden states to use. (default 10)')
     args = parser.parse_args()
-
 
     # OVERALL PROJECT ALGORITHM:
     # 1. load training and testing data into memory
@@ -111,21 +148,22 @@ def main():
     # 5+. use EM to train the HMM on the training data,
     #     output loglikelihood on train and test after each iteration
     #     if it converges early, stop the loop and print a message
-    
-    #Paths for positive and negative training data
-    postrain= os.path.join(args.train_path, 'pos')
-    negtrain= os.path.join(args.train_path, 'neg')
-    #Combine into list
-    train_paths= [postrain, negtrain]
 
-    #Create vocab and get its size. word_vocab is a dictionary from words to integers. Ex: 'painful':2070
+    # Paths for positive and negative training data
+    postrain = os.path.join(args.train_path, 'pos')
+    negtrain = os.path.join(args.train_path, 'neg')
+    # Combine into list
+    train_paths = [postrain, negtrain]
+
+    # Create vocab and get its size. word_vocab is a dictionary from words to integers. Ex: 'painful':2070
     word_vocab = build_vocab_words(train_paths)
     vocab_size = len(word_vocab)
 
-    #Create model
+    dataset = load_and_convert_data_words_to_onehot(train_paths, word_vocab)
+    # Create model
     model = HMM(args.hidden_states, vocab_size)
-    
-    
+    print(dataset)
+
 
 if __name__ == '__main__':
     main()
